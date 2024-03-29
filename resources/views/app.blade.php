9abfd358688c5ff2b5284be8625ea116f3e47f29
dpal1487 <dpal1487@gmail.com>
2024-03-29 16:57:31 +0530

    dashboard

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,10 @@ This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
       <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Google Font: Source Sans Pro -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
