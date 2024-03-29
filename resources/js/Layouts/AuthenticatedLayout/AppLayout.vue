cb2d8b2538b081f8e2b3f44c55be73f666ffda14
dpal1487 <dpal1487@gmail.com>
2024-03-30 01:09:16 +0530

    breadcrumb

--- resources/js/Layouts/AuthenticatedLayout/AppLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout/AppLayout.vue
@@ -1,110 +1,66 @@
-
-
 <script setup>
-    import AppHeader from '@/Layouts/AuthenticatedLayout/AppHeader.vue';
-    import AppSidebar from '@/Layouts/AuthenticatedLayout/AppSidebar.vue';
-    import AppLeftSidebar from '@/Layouts/AuthenticatedLayout/AppLeftSidebar.vue';
-    import AppFooter from '@/Layouts/AuthenticatedLayout/AppFooter.vue';
+import AppHeader from "@/Layouts/AuthenticatedLayout/AppHeader.vue";
+import AppSidebar from "@/Layouts/AuthenticatedLayout/AppSidebar.vue";
+import AppLeftSidebar from "@/Layouts/AuthenticatedLayout/AppLeftSidebar.vue";
+import AppFooter from "@/Layouts/AuthenticatedLayout/AppFooter.vue";
+
+defineProps({
+    title: {
+        type: String,
+        default: "Home",
+    },
+});
 </script>
 
 <template>
-  <div class="wrapper">
+    <div class="wrapper">
         <AppHeader />
-    <!-- Main Sidebar Container -->  
+        <!-- Main Sidebar Container -->
         <AppSidebar />
 
-    <!-- Content Wrapper. Contains page content -->
-         <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Starter Page</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-gray-700">{{ title }}</h1>
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item">
+                                    <a href="#">Home</a>
+                                </li>
+                               
+                                <slot name="breadcrumb" />
+                            </ol>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-sm-6">
+                            <slot name="toolbar" />
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.container-fluid -->
             </div>
+            <!-- /.content-header -->
 
-            <div class="card card-primary card-outline">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div><!-- /.card -->
-          </div>
-          <!-- /.col-md-6 -->
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
+            <!-- Main content -->
+            <div class="content">
+                <div class="container-fluid">
+                    <slot />
+                    <!-- /.content -->
+                </div>
             </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <!-- /.col-md-6 -->
         </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
-  </div>
-     <!-- /.content-wrapper -->
+        <!-- /.content-wrapper -->
 
-    <!-- Control Sidebar -->
+        <!-- Control Sidebar -->
         <AppLeftSidebar />
-    <!-- /.control-sidebar -->
+        <!-- /.control-sidebar -->
 
-    <!-- Main Footer -->
-        <AppFooter />  
-</div>
+        <!-- Main Footer -->
+        <AppFooter />
+    </div>
 </template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,26 +4,28 @@ This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
     <!-- Google Font: Source Sans Pro -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <!-- Font Awesome Icons -->
-    @routes    
+    @routes
     @vite(['resources/css/app.css', 'resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
 
 </head>
 
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
     @inertia
     @env('local')
     @endenv
     <script>
         window._asset = '{{asset(" / ")}}';
-    </script>    
+    </script>
 </body>
+
 </html>
